68ef5bf72527dab32d6ef39b21341540a67857cc
Juan Camilo Sanchez Mendez <Juancamilosanchezmendez@gmail.com>
2025-09-11 21:38:31 -0500

    feat: Implement factorial calculation and user interaction in loops.java

---
+++ loops.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // 1.
+        while (true) {
+
+            int number;
+            int factorial = 1;
+
+            System.out.println("Ingresa el numero del factorial deseado: \n");
+            if (sc.hasNextInt()) {
+
+                number = sc.nextInt();
+
+                for (int i = 1; i <= number; i++) {
+                    factorial *= i;
+                }
+
+                System.out.println("El resultado de " + number + "! es: " + factorial);
+
+            } else {
+                System.out.println("\nIngresa un numero valido");
+            }
+
+            // 2.
+
+            char[] volwels = {'a', 'e', 'i', 'o', 'u'};
+
+            String selected = (String) JOptionPane.showInputDialog(null, "Selecciona una opción:",
+                    "Select", JOptionPane.QUESTION_MESSAGE, null, volwels, volwels[0]);
+
+
+
+            String confirm;
+            do {
+                System.out.println("\nDesea volver a ejecutarlo? (si/no): ");
+                confirm = sc.nextLine();
+                if (!confirm.equals("si") && !confirm.equals("no")) {
+                    System.out.println("\ningrese una opcion valida");
+                }
+            } while (!confirm.equals("si") && !confirm.equals("no"));
+
+            if (confirm.equals("no")) {
+                System.out.println("\nHasta luego!");
+                break;
+            }
+            sc.close();
+        }
+
+
+
+    }
+}
