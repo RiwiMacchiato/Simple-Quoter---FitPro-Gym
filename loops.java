f5b4b2803a744da48071d541953b78a19a7d645b
Juan Camilo Sanchez Mendez <Juancamilosanchezmendez@gmail.com>
2025-09-12 20:21:40 -0500

    feat: Enhance user interaction for guessing game and average calculation

--- loops.java
+++ loops.java
@@ -30,11 +30,75 @@ public class loops {
 
             // 2.
             Random rand = new Random();
-            int secretNumber;
-            int maximumAttempts;
-            int attemptsUsed;
-            boolean correct;
 
+            int secretNumber = rand.nextInt(100) + 1;
+            int maximumAttempts = 10;
+            int attemptsUsed = 0;
+            boolean correct = false;
+            int attempts;
+
+            System.out.println(secretNumber);
+            System.out.println("\nAdivina el numero, tienes " + maximumAttempts
+                    + " intentos para logar adivinarlo\n");
+
+            while (attemptsUsed < maximumAttempts && !correct) {
+                System.out.println("\nIntroduce un numero: ");
+                if (!sc.hasNextInt()) {
+                    System.out.println("\nIngrese un numero valido");
+                    sc.next();
+                    continue;
+                }
+
+                attempts = sc.nextInt();
+                attemptsUsed++;
+
+                if (attempts == secretNumber) {
+                    correct = true;
+                    System.out.println("\nGanaste, el numero el numero secreto es " + secretNumber
+                            + "lo lograste en " + attemptsUsed + "intentos\n");
+                } else if (secretNumber > attempts) {
+                    System.out.println("\nEl numero secreto es mayor que " + attempts
+                            + ", te quedan " + attemptsUsed + "intentos");
+                } else {
+                    System.out.println("\nEl numero secreto es menor que " + attempts
+                            + ", te quedan " + attemptsUsed + "intentos");
+                }
+
+                if (!correct) {
+                    System.out.println(
+                            "\nSe acabaron los intentos, el numero correcto era: " + secretNumber);
+                }
+
+            }
+
+            // 3.
+
+            int sum = 0;
+            int counter = 0;
+            double average;
+
+            System.out.println(
+                    "\nIntroduce numeros para conseguir la media, introduce el numero '0' para finalizar");
+            while (true) {
+                if (!sc.hasNextInt()) {
+                    System.out.println("\nIngrese un numero valido");
+                    sc.next();
+                    continue;
+                }
+
+                number = sc.nextInt();
+                if (number == 0) {
+                    break;
+                }
+
+                sum += number;
+                counter++;
+            }
+
+            if (counter > 0) {
+                average = (double) sum / counter;
+                System.out.println(average);
+            }
 
 
             // char[] volwels = {'a', 'e', 'i', 'o', 'u'};
@@ -56,9 +120,9 @@ public class loops {
 
             if (confirm.equals("no")) {
                 System.out.println("\nHasta luego!");
+                sc.close();
                 break;
             }
-            sc.close();
         }
 
 
